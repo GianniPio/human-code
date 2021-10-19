0a2dd77c834ab4497dbd1b02375469106f02d323
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-19 15:59:44 +0200

    bonus finished

--- bonus.js
+++ bonus.js
@@ -1,1 +1,26 @@
 // Attraversare le strisce pedonali
+
+Arrivo sul bordo del marciapiede
+
+? if c'è il semaforo 
+
+    ? if il semaforo è verde
+        - Attraverso la strada
+
+    ? else if il semaforo è giallo 
+        - attraverso con più attenzione
+    
+    ? else il semaforo è rosso
+        - mi sto fermo
+        - aspetto che il semaforo diventi verde
+
+
+else attraverso la strada con attenzione
+    
+    ? if la strada è a senso unico
+        - guardo a destra or sinistra finche non passa nessuno
+        - attraverso la strada con attenzione
+
+    else
+      - guardo a destra and sinistra finche non passa nessuno
+      - attraverso la strada con attenzione
