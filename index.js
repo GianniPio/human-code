5c4a3714777d7ac7afc2d7eb0928b1e99e37faea
GianniPio <giannipiopellegrino00@gmail.com>
2021-10-19 15:37:30 +0200

    afternoon project passages

---
+++ index.js
@@ -0,0 +1,73 @@
+// Simulatore svolgimento esercizi pomeridiani
+
+Mi siedo sulla sedia:
+    - sposto la sedia indietro
+    - mi siedo
+    - mi spingo in avanti verso la scrivania
+
+Guardo il PC 
+
+    ? if il PC è spento
+        - Premo il tasto per accenderlo
+        - Aspetto che si accenda
+
+Clicco sull'icona di Slack
+
+Vado nel canale Comunicazioni
+
+Copio la repo:
+    - leggo la comunicazione
+    - individuo la repo
+    - seleziono la repo
+
+Clicco sull'icona di Chrome
+
+Vado nel mio account di GitHub
+
+Apro una nuova repository:
+    - Clicco nella sezione "Repositories"
+    - Clicco il bottone "New"
+
+
+Incollo la repo e salvo
+
+Clicco sull'icona di Visual Studio CODE
+    - Riduco ad icona Chrome
+
+Clono la Repository da GitHub
+    - seleziono la repository
+    - seleziono la cartella in cui salvare la repository
+
+Apro un file nella nuova cartella
+    - apro un file con estenzione .html
+
+        ? if serve un file CSS 
+        - apro un file con estensione .css
+
+Inizio a lavorare sull'esercizio
+    - guardo gli esempi sugli screenshot
+    - scrivo codici
+    - ogni 30 minuti salvo il progetto finchè non finisco
+        - aggiungo modifiche
+        - spiego il lavoro nella commit
+        - Eseguo push
+
+Controllo se il progetto è stato salvato
+    - Clicco su Chrome
+    - Vado su GitHub
+    - Controllo sul mio account
+
+Spengo il PC
+
+
+
+
+
+
+
+
+
+
+
+
+
